8d5b7cbc3d400cc9347b980cf8f42041822e535e
francescouni0 <f.urso3@studenti.unipi.it>
2023-02-17 12:02:59 +0100

    Creazione delle funzioni di lettura del dataset e di apertura dei file di immagini a seconda del tipo

--- reading.py
+++ reading.py
@@ -1,13 +1,44 @@
 import os
 import numpy as np
+import nibabel as nib
 
-file=[]
+#La funzione scorre tutte le cartelle e seleziona i file nelle cartelle e sottocartelle
+#selezionate, restituisce un array di paths per i file della sottocartella
+def data_path(dir,subdir):
+    r = []
+    a = []
 
-for root, subfolders, filenames in os.walk("Diffusion_parameters_maps-20230215T134959Z-001"):
-    file=np.append(file,filenames)
-    #print(filenames)
+    for root, dirs, files in os.walk(dir):    
+            for name in files:
+                r.append(os.path.join(root, name))
+            
+    
+    for i, word in enumerate(r):
+        
+        if subdir in word:
+            a.append(r[i])
+            
+                    
+                    
+    return a
 
+paths_AD= data_path("Diffusion_parameters_maps-20230215T134959Z-001","corrected_AD_image")
+paths_MD= data_path("Diffusion_parameters_maps-20230215T134959Z-001","corrected_MD_image")
+paths_RD= data_path("Diffusion_parameters_maps-20230215T134959Z-001","corrected_RD_image")
+paths_FA= data_path("Diffusion_parameters_maps-20230215T134959Z-001","corrected_FA_image")
 
-print(len(file))
-print(file[0:2])
+#Prende un array di paths e fa il caricamento delle immagini, restituisce un array
+#di oggetti nib
+def load(a):
+    f=[]
+    for i,word in enumerate(a):
+        
+        f.append(nib.load(a[i]))
+        
+    return  f
+    
+
+imgs=load(paths_AD)
+print(imgs[0])
+
 
