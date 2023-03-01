b0e80b87e99bac42f07cf224292fa7b4209368be
simonedamiani <114066103+simonedamiani@users.noreply.github.com>
2023-03-01 18:10:49 +0100

    New FE

--- feature_extractor.m
+++ feature_extractor.m
@@ -1,38 +1,45 @@
 function [Regions, Means, Stds] = feature_extractor(image_filepaths, segmentation_filepaths)
 
-
 opts = detectImportOptions('FreeSurferColorLUT.txt');
 opts.VariableNames = {'Index', 'Name', 'R', 'G', 'B', 'A'};
 opts.CommentStyle = '#';
 labels = readtable("FreeSurferColorLUT.txt", opts);
 
-Means = struct();
-Stds = struct();
+indices = labels.Index;
+
+for i = 1:1:length(image_filepaths)
+
+    segmentation = niftiread(string(segmentation_filepaths(i)));
+    pixel_values = unique(segmentation);
+    indices = intersect(indices, pixel_values);
+
+end
+
+[~, pos] = intersect(labels.Index, indices);
+Regions = string(labels.Name(pos));
+
+Means = [NaN; indices];
+Stds = [NaN; indices];
 
 for i = 1:1:length(image_filepaths)
     
     image = niftiread(string(image_filepaths(i)));
     segmentation = niftiread(string(segmentation_filepaths(i)));
 
-    indices = unique(segmentation);
-
-    [~, pos] = intersect(labels.Index, indices);
-    region = string(labels.Name(pos));
-    f_mean = [];
-    f_std = [];
+    f_mean = [i];
+    f_std = [i];
 
     for j = 1:1:length(indices)
 
         bool_mask = segmentation == indices(j);
         results = image(bool_mask);
-        f_mean = [f_mean mean(results)];
-        f_std = [f_std std(results)];
+        f_mean = [f_mean; mean(results)];
+        f_std = [f_std; std(results)];
 
     end
 
-Regions.("Subject_"+i) = region;
-Means.("Subject_"+i) = f_mean.';
-Stds.("Subject_"+i) = f_std.';
+Means = [Means f_mean];
+Stds = [Stds f_std];
 
 end
 
