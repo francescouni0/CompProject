ddb1d8c45addfc7d37b6533174826f76c754f327
simonedamiani <114066103+simonedamiani@users.noreply.github.com>
2023-03-01 18:11:00 +0100

    Old FE

---
+++ feature_extractor_old.m
@@ -0,0 +1,39 @@
+function [Regions, Means, Stds] = feature_extractor_old(image_filepaths, segmentation_filepaths)
+
+
+opts = detectImportOptions('FreeSurferColorLUT.txt');
+opts.VariableNames = {'Index', 'Name', 'R', 'G', 'B', 'A'};
+opts.CommentStyle = '#';
+labels = readtable("FreeSurferColorLUT.txt", opts);
+
+Means = struct();
+Stds = struct();
+
+for i = 1:1:length(image_filepaths)
+    
+    image = niftiread(string(image_filepaths(i)));
+    segmentation = niftiread(string(segmentation_filepaths(i)));
+
+    indices = unique(segmentation);
+
+    [~, pos] = intersect(labels.Index, indices);
+    region = string(labels.Name(pos));
+    f_mean = [];
+    f_std = [];
+
+    for j = 1:1:length(indices)
+
+        bool_mask = segmentation == indices(j);
+        results = image(bool_mask);
+        f_mean = [f_mean mean(results)];
+        f_std = [f_std std(results)];
+
+    end
+
+Regions.("Subject_"+i) = region;
+Means.("Subject_"+i) = f_mean.';
+Stds.("Subject_"+i) = f_std.';
+
+end
+
+end
