b4cd9fc95c905a350d9e63217cbf840229f06070
francescouni0 <f.urso3@studenti.unipi.it>
2023-03-15 14:23:14 +0100

    Aggiunta alberi classifiers

--- classifiers.py
+++ classifiers.py
@@ -1,14 +1,14 @@
 from sklearn.pipeline import Pipeline
 from sklearn.decomposition import PCA
 from sklearn.metrics import RocCurveDisplay
-from sklearn.tree import export_graphviz
-from IPython.display import Image
-import graphviz
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, confusion_matrix, precision_score, recall_score, ConfusionMatrixDisplay
 from sklearn.model_selection import RandomizedSearchCV, train_test_split
 from scipy.stats import randint
 import matplotlib.pyplot as plt
+from sklearn.tree import export_graphviz
+from IPython.display import Image
+import graphviz
 
 
 
@@ -16,10 +16,17 @@ import matplotlib.pyplot as plt
 param_dist = {'n_estimators': randint(50,500),
               'max_depth': randint(1,20)}
 
-def RFPipeline_noPCA(X_tr,y_tr,X_tst,y_tst,n_iter,cv):
+def RFPipeline_noPCA(a,c,n_iter,cv):
+    
 
+#a and c should be pandas dataframe
 
+    
+    X=a.values
+    y=c.values
+    region = list(a.columns.values)
 
+    X_tr, X_tst, y_tr, y_tst = train_test_split(X, y, test_size=.1, random_state=6)
 
     pipeline_simple = Pipeline(steps=[("hyper_opt", RandomizedSearchCV(RandomForestClassifier(), param_distributions = param_dist, n_iter=n_iter, cv=cv))])
     pipeline_simple.fit(X_tr, y_tr)
@@ -41,6 +48,14 @@ def RFPipeline_noPCA(X_tr,y_tr,X_tst,y_tst,n_iter,cv):
 
 
     plt.show()
+    
+    
+    
+    for i in range(3):
+        tree = pipeline_simple.best_estimator_[i]
+        dot_data = export_graphviz(tree,feature_names=region[1:],  filled=True, impurity=False, proportion=True,class_names=["CN","AD"])
+        graph = graphviz.Source(dot_data)
+        display(graph)
 
 
     return pipeline_simple.fit
@@ -48,10 +63,19 @@ def RFPipeline_noPCA(X_tr,y_tr,X_tst,y_tst,n_iter,cv):
 
 
 
-def RFPipeline_PCA(X_tr,y_tr,X_tst,y_tst,n_iter,cv):
+def RFPipeline_PCA(a,c,n_iter,cv):
 
 
+#a and c should be pandas dataframe
 
+    X=a.values
+    y=c.values
+
+
+    region = list(a.columns.values)
+
+
+    X_tr, X_tst, y_tr, y_tst = train_test_split(X, y, test_size=.1, random_state=6)
 
     pipeline_PCA = Pipeline(steps=[("dim_reduction", PCA()),("hyper_opt", RandomizedSearchCV(RandomForestClassifier(), param_distributions = param_dist, n_iter=n_iter, cv=cv))])
     pipeline_PCA.fit(X_tr, y_tr)
@@ -73,5 +97,11 @@ def RFPipeline_PCA(X_tr,y_tr,X_tst,y_tst,n_iter,cv):
 
 
     plt.show()
+    
+    for i in range(3):
+        tree = pipeline_PCA.best_estimator_[i]
+        dot_data = export_graphviz(tree,feature_names=region[1:],  filled=True, impurity=False, proportion=True,class_names=["CN","AD"])
+        graph = graphviz.Source(dot_data)
+        display(graph)
 
     return pipeline_PCA.fit
