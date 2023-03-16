c4797f462e3c43a1403cb13bb1c9baf5ffebf585
francescouni0 <f.urso3@studenti.unipi.it>
2023-03-16 10:13:41 +0100

    aggiunti para grid distinti per linear e rbf

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from classifiers import RFPipeline_noPCA, RFPipeline_PCA , SVMPipeline_feature_reduction
+from classifiers import RFPipeline_noPCA, RFPipeline_PCA , SVMPipeline
 from sklearn.model_selection import train_test_split
 
 
@@ -10,10 +10,18 @@ from feature_extractor import feature_extractor
 
 
 paths_FA= reading.data_path("Diffusion_parameters_maps-20230215T134959Z-001","corrected_FA_image")
+
+paths_MD= reading.data_path("Diffusion_parameters_maps-20230215T134959Z-001","corrected_MD_image")
+
 paths_masks=reading.data_path("Diffusion_space_segmentations-20230215T134839Z-001","Diffusion_space_segmentations-20230215T134839Z-001")
 
-a, b ,c=feature_extractor(paths_FA,paths_masks)
+mean_fa, std_fa ,group=feature_extractor(paths_FA,paths_masks)
+
+mean_md, std_md ,_=feature_extractor(paths_FA,paths_masks)
+
 
 #RFPipeline_noPCA(a,c,10,5)
 
-SVMPipeline_feature_reduction(a,c,"linear")
+SVMPipeline(mean_fa,group,"linear")
+
+SVMPipeline(mean_md,group,"rbf")
