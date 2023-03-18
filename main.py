5567eed976bbe1cfd51321fc7871bdbc100e8d02
simonedamiani <114066103+simonedamiani@users.noreply.github.com>
2023-03-18 18:35:16 +0100

    FE added and PEP8 corrections

--- main.py
+++ main.py
@@ -1,22 +1,33 @@
-import classifiers 
-import os
-import numpy as np
+import classifiers
 import reading 
-from feature_extractor import feature_extractor
+from feature_extractor import feature_extractor, feature_extractor_par
 import argparse
+
 
 if __name__ == '__main__':
     
     parser = argparse.ArgumentParser(
-        description="ML classifiers analysis for Alzheimer's desease detection on Diffusion Images."
+        description="ML and DL classifiers analysis for Alzheimer's disease detection on Diffusion Images."
     )
-    
+
+    parser.add_argument(
+        "-fe",
+        "--featureextractor",
+        metavar="",
+        type=str,
+        choices=[
+            'Sequential',
+            'Parallel'
+        ],
+        help="Type of feature extractor to use",
+        default="Sequential",
+    )
 
     parser.add_argument(
         "-dpd",
         "--datapathdiff",
         metavar="",
-        help="path of the diffusion parameters map",
+        help="Path of the diffusion parameters map",
         default="Diffusion_parameters_maps-20230215T134959Z-001"
     )
     
@@ -24,7 +35,7 @@ if __name__ == '__main__':
         "-dpm",
         "--datapathmask",
         metavar="",
-        help="path of the segmentetion map",
+        help="Path of the segmentation map",
         default="Diffusion_space_segmentations-20230215T134839Z-001"
     )
     
@@ -53,84 +64,77 @@ if __name__ == '__main__':
         help="Type of ML classifier to use",
         default="RF",
     )
-    
-     
-    
+
     args = parser.parse_args()
 
-
-
-    
-    
     PATH_diff = args.datapathdiff
     
     PATH_masks = args.datapathmask
-    
-    paths_masks=reading.data_path(PATH_masks,"Diffusion_space_segmentations-20230215T134839Z-001")
+    paths_masks = reading.data_path(PATH_masks, "Diffusion_space_segmentations-20230215T134839Z-001")
 
-    if args.datatype=="MD":
-        
-        path_subdir="corrected_MD_image"
-        
-        paths_MD= reading.data_path(PATH_diff,path_subdir)
-        
-        mean_md, std_md ,group=feature_extractor(paths_MD,paths_masks)
-        
-        if args.classifier=="Random Forest":
-            
-            option1=input("Principal component analysis? Y or N:    ")
-            
-            if option1=="n":
-            
-                classifiers.RFPipeline_noPCA(mean_md,group,10,5)
-                
-            if option1=="y":
-                
-                classifiers.RFPipeline_PCA(mean_md,group,10,5)
-            else:
-                print('Repeat')
-            
-        elif args.classifier=="Support Vector Machines": 
-                
-            classifiers.SVMPipeline(mean_md,group,"rbf")
-                
-            
-            
+    if args.datatype == "MD":
+        path_subdir = "corrected_MD_image"
+        paths_MD = reading.data_path(PATH_diff, path_subdir)
 
-    elif args.datatype=="FA":
-        
-        path_subdir="corrected_FA_image"
-        
-        paths_FA= reading.data_path(PATH_diff,path_subdir)
-        
-        mean_fa, std_fa ,group=feature_extractor(paths_FA,paths_masks)
-        if args.classifier=="Random Forest":
-            
-            option1=input("Principal component analysis? Y or N:    ")
-            
-            if option1=="n":
+        if args.featureextractor == "Sequential":
+            mean_md, std_md, group = feature_extractor(paths_MD, paths_masks)
+        elif args.featureextractor == "Parallel":
+            mean_md, std_md, group = feature_extractor_par(paths_MD, paths_masks)
+        else:
+            print('Input not valid, please retry.')
 
-                classifiers.RFPipeline_noPCA(mean_fa,group,10,5)
-                
-            elif option1=="y":
-                
-                classifiers.RFPipeline_PCA(mean_fa,group,10,5)
-            else:
-                print('Repeat')
+        if args.classifier == "Random Forest":
+            option1 = input("Principal component analysis? Y or N: ")
             
-        elif args.classifier=="Support Vector Machines": 
-    
-            option2=input("Feature Reduction? (approx time 6min) Y or N:    ")
-            
-            if option2=="n":
-                
-                classifiers.SVMPipeline(mean_fa,group,"rbf")
-                
-            elif option2=="y":
-                
-                classifiers.SVMPipeline_feature_red(mean_fa,group)
+            if option1.lower()[0] == "n":
+                classifiers.RFPipeline_noPCA(mean_md, group, 10, 5)
+            elif option1.lower()[0] == "y":
+                classifiers.RFPipeline_PCA(mean_md, group, 10, 5)
             else:
-                print('Repeat')
+                print('Input not valid, please retry.')
+            
+        elif args.classifier == "Support Vector Machines":
+            classifiers.SVMPipeline(mean_md, group, "rbf")
+
+        else:
+            print('Input not valid, please retry.')
+
+    elif args.datatype == "FA":
+        path_subdir = "corrected_FA_image"
+        paths_FA = reading.data_path(PATH_diff, path_subdir)
+        
+        if args.featureextractor == "Sequential":
+            mean_fa, std_fa, group = feature_extractor(paths_FA, paths_masks)
+        elif args.featureextractor == "Parallel":
+            mean_fa, std_fa, group = feature_extractor_par(paths_FA, paths_masks)
+        else:
+            print('Input not valid, please retry.')
+
+        if args.classifier == "Random Forest":
+            option1 = input("Principal component analysis? Y or N: ")
+            
+            if option1.lower()[0] == "n":
+                classifiers.RFPipeline_noPCA(mean_fa, group, 10, 5)
+            elif option1.lower()[0] == "y":
+                classifiers.RFPipeline_PCA(mean_fa, group, 10, 5)
+            else:
+                print('Input not valid, please retry.')
+            
+        elif args.classifier == "Support Vector Machines":
+            option2 = input("Feature Reduction? (approx time 6min) Y or N: ")
+            
+            if option2.lower()[0] == "n":
+                classifiers.SVMPipeline(mean_fa, group, "rbf")
+            elif option2.lower()[0] == "y":
+                classifiers.SVMPipeline_feature_red(mean_fa, group)
+            else:
+                print('Input not valid, please retry.')
+
+        else:
+            print('Input not valid, please retry.')
+
+    else:
+        print('Input not valid, please retry.')
             
         
         
