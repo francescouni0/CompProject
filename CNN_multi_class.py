4e7300bacea1260aa0437c657d329bf0b3a7a58d
francescouni0 <f.urso3@studenti.unipi.it>
2023-03-18 13:50:23 +0100

    Classe che worka e plotta

--- CNN_multi_class.py
+++ CNN_multi_class.py
@@ -14,119 +14,119 @@ import os
 from tensorflow.keras import layers
 from tensorflow.keras import losses
 from tensorflow import keras
+import tensorflow
 
 
 
-
-
-class CNN_multi(keras.Sequential):
-
+class MyModel(tensorflow.keras.Model):
     
+    def __init__(self, shape=(110, 110, 3)):
+        super(MyModel, self).__init__()
+        self.conv1 = Conv2D(8, (3,3), padding='valid', input_shape=shape, kernel_regularizer='l1')
+        self.bn1 = BatchNormalization()
+        self.act1 = Activation('relu')
+        self.pool1 = MaxPooling2D((2,2), strides=3)
+        self.drop1 = Dropout(0.3)
+        self.conv2 = Conv2D(16, (3,3), padding='valid', input_shape=shape, kernel_regularizer='l1')
+        self.bn2 = BatchNormalization()
+        self.act2 = Activation('relu')
+        self.pool2 = MaxPooling2D((3,3), strides=3)
+        self.flatten = Flatten()
+        self.fc1 = Dense(116, activation='relu')
+        self.fc2 = Dense(1, activation='sigmoid')
 
+    def call(self, inputs):
+        x = self.conv1(inputs)
+        x = self.bn1(x)
+        x = self.act1(x)
+        x = self.pool1(x)
+        x = self.drop1(x)
+        x = self.conv2(x)
+        x = self.bn2(x)
+        x = self.act2(x)
+        x = self.pool2(x)
+        x = self.flatten(x)
+        x = self.fc1(x)
+        return self.fc2(x)
+    
+    def compile_and_fit(self, x_train, y_train, x_val, y_val):
+        self.compile(optimizer=SGD(learning_rate=0.01), loss=losses.Hinge(), metrics=['accuracy'])
+        reduce_on_plateau = ReduceLROnPlateau(
+            monitor="val_loss",
+            factor=0.1,
+            patience=20,
+            verbose=0,
+            mode="auto",
+            min_delta=0.0001,
+            cooldown=0,
+            min_lr=0)
 
-    def __init__(self,images,labels):
-        super().__init__()
-        
+        early_stopping = EarlyStopping(
+            monitor="val_loss",
+            min_delta=0,
+            patience=100,
+            verbose=0,
+            mode="auto",
+            baseline=None,
+            restore_best_weights=False,
+            start_from_epoch=10)
 
-        
-        
-        
-        
-
-        
-        shape=(110, 110, 3)
- 
-       
-
-    def build_model(self):
-        
-        shape=(110, 110, 3)
-        model = Sequential([
-      
-      Conv2D(8, (3,3), padding='valid', input_shape=shape, kernel_regularizer='l1'),
-      BatchNormalization(),
-      Activation('relu'),
-      
-      MaxPooling2D((2,2), strides=3),
-      Dropout(0.3),
-      
-      Conv2D(16, (3,3), padding='valid', input_shape=shape, kernel_regularizer='l1'),
-      BatchNormalization(),
-      Activation('relu'),
-      
-      MaxPooling2D((3,3), strides=3),
-      #Dropout(0.1),
-      
-      
-      Flatten(),
-
-      Dense(116, activation='relu'),
-      #Dropout(0.1),
-      Dense(1, activation='sigmoid')
-     ])
-        model.summary()
-        model.compile(optimizer=SGD(learning_rate=0.01),loss=losses.Hinge(),metrics=['accuracy'])
-        
-        
-
-
-    def train(self, x_train, y_train):
         epochs= 1000
         batch_size = 10
+        history = self.fit(x_train, y_train,
+                            batch_size=batch_size,
+                            epochs=epochs,
+                            steps_per_epoch = round(len(x_train)/batch_size),
+                            verbose=1,
+                            validation_data=(x_val, y_val),
+                            validation_steps=round(len(x_val)/batch_size),
+                            callbacks=[reduce_on_plateau, early_stopping])
+        self.loss_plot(history), self.accuracy_roc(x_val,y_val)
+
+    def loss_plot(self, history):
+        acc = history.history['accuracy']
+        val_acc = history.history['val_accuracy']
+        loss = history.history['loss']
+        val_loss = history.history['val_loss']
+
+        epochs_range = range(1, len(acc)+1)
+        #Train and validation accuracy 
+        plt.figure(figsize=(15, 15))
+        plt.subplot(2, 2, 1)
+        plt.plot(epochs_range, acc, label='Training Accuracy')
+        plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+        plt.legend(loc='lower right')
+        plt.title('Training and Validation Accuracy')
+        #Train and validation loss 
+        plt.subplot(2, 2, 2)
+        plt.plot(epochs_range, loss, label='Training Loss')
+        plt.plot(epochs_range, val_loss, label='Validation Loss')
+        plt.legend(loc='upper right')
+        plt.title('Training and Validation Loss')
+        plt.show()
         
-        X_train, x_test, Y_train, y_test = train_test_split(images[:,:,:], labels, test_size=0.2, random_state=10)
-        x_train, x_val, y_train, y_val = train_test_split(X_train, Y_train, test_size=0.25, random_state=20)
-        
-        
-        reduce_on_plateau = ReduceLROnPlateau(
-        monitor="val_loss",
-        factor=0.1,
-        patience=20,
-        verbose=0,
-        mode="auto",
-        min_delta=0.0001,
-        cooldown=0,
-        min_lr=0)
+    def accuracy_roc(self, x_val, y_val):
+        _, val_acc = self.evaluate(x_val, y_val, verbose=0)
+        print('Validation accuracy: %.3f' % (val_acc))
     
-        early_stopping = EarlyStopping(
-        monitor="val_loss",
-        min_delta=0,
-        patience=100,
-        verbose=0,
-        mode="auto",
-        baseline=None,
-        restore_best_weights=False,
-        start_from_epoch=10)
-        
-        model = build_model(self)
-        
-        model.fit(x_train, y_train,
-        batch_size=batch_size,
-        epochs=epochs,
-        steps_per_epoch = round(len(x_train)/batch_size),
-        verbose=1,
-        validation_data=(x_val, y_val),
-        validation_steps=round(len(x_val)/batch_size),
-        callbacks=[reduce_on_plateau, early_stopping])
-
-    def predict(self, x_test):
-        X_train, x_test, Y_train, y_test = train_test_split(images[:,:,:], labels, test_size=0.2, random_state=10)
-        x_train, x_val, y_train, y_val = train_test_split(X_train, Y_train, test_size=0.25, random_state=20)
-        
-        
-        model.predict(x_test)
-
-    def validation(self, x_val, y_val):
-        X_train, x_test, Y_train, y_test = train_test_split(images[:,:,:], labels, test_size=0.2, random_state=10)
-        x_train, x_val, y_train, y_val = train_test_split(X_train, Y_train, test_size=0.25, random_state=20)        
-        
-        
-        model.evaluate(x_test,y_test, verbose=2)
-
-
-
-
-
+        preds = self.predict(x_val, verbose=1)
+        # Compute Receiver operating characteristic (ROC)
+        fpr, tpr, _ = roc_curve(y_val, preds)
+        roc_auc = auc(fpr, tpr)
+        print('AUC = %.3f'% (roc_auc))
+    
+        # Plot of a ROC curve
+        plt.figure()
+        lw = 2
+        plt.plot(fpr, tpr, color='darkorange', lw=lw, label='ROC curve (area = %0.2f)' % roc_auc)
+        plt.plot([0, 1], [0, 1], color='navy', lw=lw, linestyle='--')
+        plt.xlim([0.0, 1.0])
+        plt.ylim([0.0, 1.05])
+        plt.xlabel('False Positive Rate')
+        plt.ylabel('True Positive Rate')
+        plt.title('Validation ROC')
+        plt.legend(loc="lower right")
+        plt.show()
 
 
 
@@ -200,13 +200,18 @@ images = np.concatenate((images, images_rotated, images_zoomed, images_croped, i
 
 labels = np.concatenate((labels, labels, labels,labels,labels,labels,labels,labels,labels))
 
-
+def split_2D():
+    X_train, x_test, Y_train, y_test = train_test_split(images[:,:,:], labels, test_size=0.2, random_state=10)
+    x_train, x_val, y_train, y_val = train_test_split(X_train, Y_train, test_size=0.25, random_state=20)
+    return x_train, y_train, x_val, y_val, x_test, y_test
         
-from CNN_multi_class import CNN_multi
 
-model=CNN_multi(images,labels)
+x_train, y_train, x_val, y_val, x_test, y_test = split_2D()
 
-model.build_model()
-model.summary()
-model.train()
+shape=(110, 110, 3)
+
+model=MyModel(shape)
+
+
+model.compile_and_fit(x_train,y_train,x_val,y_val)
 
