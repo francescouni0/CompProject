dab4562c6bc7b89ab7a80d9f397f9ca4d1da58ff
simonedamiani <114066103+simonedamiani@users.noreply.github.com>
2023-03-27 18:45:02 +0200

    doc update

--- CNN_tools/CNN_multi_class.py
+++ CNN_tools/CNN_multi_class.py
@@ -1,4 +1,3 @@
-
 import sys
 from pathlib import Path
 import os
@@ -28,9 +27,53 @@ import CNN_tools.CNN_multi_utilities as CNN_multi_utilities
 
 class MyModel(tensorflow.keras.Model):
     """
+    Subclass of the TensorFlow Keras Model class. This model is a Convolutional Neural Network (CNN) 2.5D designed for
+    image classification tasks: it consists of two 2D convolutional layers followed by two fully connected layers,
+    including batch normalization, activation functions, max pooling and dropout to improve its performance and
+    generalization power.
 
+    Attributes
+    ----------
+    conv1 : tensorflow.python.keras.layers.Conv2D
+        First convolutional layer with 3x3 kernel size and 8 filters.
+    bn1 : tensorflow.python.keras.layers.BatchNormalization
+        First batch normalization layer.
+    act1 : tensorflow.python.keras.layers.Activation
+        First activation function layer with a Rectified Linear Unit function.
+    pool1 : tensorflow.python.keras.layers.MaxPooling2D
+        First max pooling layer with 2x2 pool size and stride 3.
+    drop1 : tensorflow.python.keras.layers.Dropout
+        First dropout layer with a 0.3 rate.
+    conv2 : tensorflow.python.keras.layers.Conv2D
+        Second convolutional layer with 3x3 kernel size and 16 filters.
+    bn2 : tensorflow.python.keras.layers.BatchNormalization
+        Second batch normalization layer.
+    act2 : tensorflow.python.keras.layers.Activation
+        Second activation function layer with a Rectified Linear Unit function.
+    pool2 : tensorflow.python.keras.layers.MaxPooling2D
+        Second max pooling layer with 3x3 pool size and stride 3.
+    flatten : tensorflow.python.keras.layers.Flatten
+        Flatten layer.
+    fc1 : tensorflow.python.keras.layers.Dense
+        First fully connected layer with 116 neurons and ReLU activation function.
+    fc2 : tensorflow.python.keras.layers.Dense
+        Second fully connected layer with a single neuron and sigmoid activation function for binary classification.
     """
     def __init__(self, shape=(110, 110, 3)):
+        """
+        Initializes the "MyModel" object with the given shape of the input images (default = (110, 110, 3)). It creates
+        the model architecture by defining the layers and the associated parameters.
+
+        Parameters
+        ----------
+        shape : tuple
+            Tuple containing the (x,y) dimensions of image matrix and the number of slices (or channels). Default is
+            (110. 110, 3).
+
+        Returns
+        -------
+            None
+        """
         super(MyModel, self).__init__()
         self.conv1 = Conv2D(8, (3, 3), padding='valid', input_shape=shape, kernel_regularizer='l1')
         self.bn1 = BatchNormalization()
@@ -46,6 +89,21 @@ class MyModel(tensorflow.keras.Model):
         self.fc2 = Dense(1, activation='sigmoid')
 
     def call(self, inputs):
+        """
+        Implements the forward pass of the model, taking the input images and feeding them through each layer of the CNN.
+        The order of execution is as specified in the constructor.
+
+        Parameters
+        ----------
+        inputs : numpy.ndarray
+            Input DTI images to the CNN.
+
+        Returns
+        -------
+        self.fc2 : numpy.ndarray
+            A numpy array of shape (n_samples, 1) representing binary classification prediction for each input sample,
+            where n_samples is the number of input images.
+        """
         x = self.conv1(inputs)
         x = self.bn1(x)
         x = self.act1(x)
@@ -61,6 +119,34 @@ class MyModel(tensorflow.keras.Model):
         return self.fc2(x)
     
     def compile_and_fit(self, x_train, y_train, x_val, y_val, x_test, y_test, n_epochs, batchsize):
+        """
+        Compiles the model and fits it to the training data. Once fitted, the training process is shown by loss and
+        accuracy plots (both for training and validation) and the overall performance is evaluated on the validation and
+        test set.
+
+        Parameters
+        ----------
+        x_train : numpy.ndarray
+            Train subset of DTI images.
+        y_train : numpy.ndarray
+            Labels corresponding to x_train.
+        x_val : ndarray
+            Validation subset of DTI images.
+        y_val : numpy.ndarray
+            Labels corresponding to x_val.
+        x_test : numpy.ndarray
+            Test subset of DTI images.
+        y_test : numpy.ndarray
+            Labels corresponding to x_test.
+        n_epochs : int
+            Maximum number of epochs to train the models.
+        batchsize : int
+            Batch size to use during training.
+
+        Returns
+        -------
+            None
+        """
         self.compile(optimizer=SGD(learning_rate=0.01), loss=losses.Hinge(), metrics=['accuracy'])
 
         reduce_on_plateau = ReduceLROnPlateau(monitor="val_loss",
@@ -99,6 +185,19 @@ class MyModel(tensorflow.keras.Model):
         self.save_weights(Path('model.h5'))
 
     def accuracy_loss_plot(self, history):
+        """
+        Generates two plots: one of model's accuracy on the training and validation sets, the other of model's loss on
+        the same sets of data.
+
+        Parameters
+        ----------
+        history : keras.callbacks.History
+            History object generated during model training.
+
+        Returns
+        -------
+            None
+        """
         acc = history.history['accuracy']
         val_acc = history.history['val_accuracy']
         loss = history.history['loss']
@@ -123,11 +222,26 @@ class MyModel(tensorflow.keras.Model):
         plt.show()
         
     def validation_roc(self, x_val, y_val):
+        """
+        Generates a plot of the ROC curve and calculates the AUC score for the model on the validation set.
+
+        Parameters
+        ----------
+        x_val : ndarray
+            Validation subset of DTI images.
+        y_val : numpy.ndarray
+            Labels corresponding to x_val.
+
+        Returns
+        -------
+            None
+        """
         _, val_acc = self.evaluate(x_val, y_val, verbose=0)
         print('Validation accuracy: %.3f' % val_acc)
     
         preds = self.predict(x_val, verbose=1)
-        # Compute Receiver operating characteristic (ROC)
+
+        # Compute Receiver operating characteristic (ROC) curve
         fpr, tpr, _ = roc_curve(y_val, preds)
         roc_auc = auc(fpr, tpr)
         print('AUC = %.3f' % roc_auc)
@@ -146,14 +260,31 @@ class MyModel(tensorflow.keras.Model):
         plt.show()
         
     def test_roc(self, x_test, y_test):
+        """
+        Generates a plot of the ROC curve and calculates the AUC score for the model on the test set.
+
+        Parameters
+        ----------
+        x_test : ndarray
+            Test subset of DTI images.
+        y_test : numpy.ndarray
+            Labels corresponding to x_test.
+
+        Returns
+        -------
+            None
+        """
         test_loss, test_acc = self.evaluate(x_test, y_test)
         print('\nTest accuracy: %.3f' % test_acc)
         
         preds_test = self.predict(x_test, verbose=1)
+
+        # Compute Receiver operating characteristic (ROC) curve
         fpr, tpr, _ = roc_curve(y_test, preds_test)
         roc_auc = auc(fpr, tpr)
         print('AUC = %.3f' % roc_auc)
-        
+
+        # Plot of a ROC curve
         plt.figure()
         lw = 2
         plt.plot(fpr, tpr, color='darkorange', lw=lw, label='ROC curve (area = %0.2f)' % roc_auc)
@@ -167,6 +298,34 @@ class MyModel(tensorflow.keras.Model):
         plt.show()
     
     def load(self, path, x_train, y_train, x_val, y_val, x_test, y_test, n_epochs, batchsize):
+        """
+        Loads, compiles and re-trains a previously trained model stored in a H5 file.
+
+        Parameters
+        ----------
+        path : str
+            Path to the H5 file storing the pre-trained model.
+        x_train : numpy.ndarray
+            Train subset of DTI images.
+        y_train : numpy.ndarray
+            Labels corresponding to x_train.
+        x_val : ndarray
+            Validation subset of DTI images.
+        y_val : numpy.ndarray
+            Labels corresponding to x_val.
+        x_test : numpy.ndarray
+            Test subset of DTI images.
+        y_test : numpy.ndarray
+            Labels corresponding to x_test.
+        n_epochs : int
+            Maximum number of epochs to train the models.
+        batchsize : int
+            Batch size to use during training.
+
+        Returns
+        -------
+            None
+        """
         self.compile(optimizer=SGD(learning_rate=0.01), loss=losses.Hinge(), metrics=['accuracy'])
         self.train_on_batch(x_train, y_train)
         self.load_weights(path)
