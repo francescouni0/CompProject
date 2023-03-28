b7d6980cba1700c0e2340aa9e46306e29ca21dcb
francescouni0 <f.urso3@studenti.unipi.it>
2023-03-28 18:40:07 +0200

    creato classe che moccka matlab_mock

---
+++ test/matlab_mock.py
@@ -0,0 +1,12 @@
+from unittest.mock import MagicMock
+import sys
+
+sys.path.insert(0, str(Path(os.getcwd()).parent))
+
+
+class MockMatlabEngine(MagicMock):
+    @classmethod
+    def connect(cls, *args, **kwargs):
+        return cls()
+
+sys.modules['matlab.engine'] = MockMatlabEngine
