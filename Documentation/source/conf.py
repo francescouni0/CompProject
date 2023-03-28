27f6bde5195c6c89be2c738b8af6cb1cfe3dcf74
simonedamiani <114066103+simonedamiani@users.noreply.github.com>
2023-03-28 16:37:23 +0200

    minor changes

--- Documentation/source/conf.py
+++ Documentation/source/conf.py
@@ -12,7 +12,7 @@ sys.path.insert(0, os.path.abspath('../..'))
 
 project = 'CompProject'
 copyright = '2023, Simone Damiani & Francesco Urso'
-author = 'SSimone Damiani & Francesco Urso'
+author = 'Simone Damiani & Francesco Urso'
 release = '0.1.0'
 
 # -- General configuration ---------------------------------------------------
