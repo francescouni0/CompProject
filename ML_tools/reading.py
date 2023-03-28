749e4c41ab4b8fca55df1ced7ccfc5a6f6d6345e
simonedamiani <114066103+simonedamiani@users.noreply.github.com>
2023-03-28 16:07:38 +0200

    new fun (re)added

--- ML_tools/reading.py
+++ ML_tools/reading.py
@@ -1,8 +1,7 @@
-import os
-import nibabel as nib
 import sys
 from pathlib import Path
 import os
+import re
 
 sys.path.insert(0, str(Path(os.getcwd()).parent))
 os.chdir('..')
@@ -43,4 +42,33 @@ def data_path(dir, subdir):
     return filepaths
 
 
+def data_path_general(dir, subdir):
+    """
+    Creates a list collecting absolute paths to the files contained in a sub-folder of a parent folder.
 
+    Parameters
+    ----------
+    dir : str
+        Name of the parent folder.
+    subdir : str
+        Name of the parent folder.
+
+    Returns
+    -------
+    filepaths : list
+        Paths to the files contained in the specified sub-folder.
+    """
+    roots = []
+    filepaths = []
+
+    for root, dirs, files in os.walk(dir):
+        for name in files:
+            roots.append(os.path.join(root, name))
+
+    for i, word in enumerate(roots):
+        if subdir in word:
+            filepaths.append(roots[i])
+
+    filepaths.sort(key=lambda x: int(re.findall(r"\d+", os.path.basename(x))[1]))
+
+    return filepaths
